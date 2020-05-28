019865543c660f2de5d391ad90259e86f38c874c
shandysiswandi <shandysiswandi@gmail.com>
2020-05-28 23:37:36 +0700

    update app

--- app.ts
+++ app.ts
@@ -1,13 +1,17 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
 import { router } from "./routes/router.ts";
 import { client } from "./config/database.ts";
 
-const app = new Application();
+const app: Application = new Application();
+const env = config();
 
 app.use(router.routes());
 app.use(router.allowedMethods());
+
 await client.connect();
 
-await app.listen({ port: 8000 });
+console.log(`Listening on ${env.HOST}:${env.PORT}`);
+await app.listen({ port: parseInt(env.PORT) });
 
 await client.end();
