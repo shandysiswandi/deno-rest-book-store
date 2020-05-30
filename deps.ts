39f84dcb2ced01d07269b145509e00d0e4c81448
shandysiswandi <shandysiswandi@gmail.com>
2020-05-31 00:44:09 +0700

    add deps to get all module from public source

---
+++ deps.ts
@@ -0,0 +1,7 @@
+export { config } from "https://deno.land/x/dotenv/mod.ts";
+export { Client } from "https://deno.land/x/postgres/mod.ts";
+export {
+  Application,
+  Router,
+  RouterContext,
+} from "https://deno.land/x/oak/mod.ts";
