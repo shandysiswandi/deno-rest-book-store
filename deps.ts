b45b3cd255f59d395dfc832e475a66f96d1a468d
shandysiswandi <shandysiswandi@gmail.com>
2020-05-31 19:05:38 +0700

    add module JWT

--- deps.ts
+++ deps.ts
@@ -1,7 +1,22 @@
+// dotenv
 export { config } from "https://deno.land/x/dotenv/mod.ts";
+
+// postgresql
 export { Client } from "https://deno.land/x/postgres/mod.ts";
+
+// OAK
 export {
   Application,
+  Context,
   Router,
   RouterContext,
 } from "https://deno.land/x/oak/mod.ts";
+
+// JWT
+export { validateJwt } from "https://deno.land/x/djwt/validate.ts";
+export {
+  makeJwt,
+  setExpiration,
+  Jose,
+  Payload,
+} from "https://deno.land/x/djwt/create.ts";
