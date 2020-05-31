5cdfc4d245d32e06190e58667a3f11dbd9b794de
shandysiswandi <shandysiswandi@gmail.com>
2020-06-01 03:11:12 +0700

    update handler function

--- handlers/errors.ts
+++ handlers/errors.ts
@@ -1,10 +1,11 @@
-import { Response } from "../deps.ts";
+import { Context } from "../deps.ts";
 
-export async function errorHandler(ctx: { response: Response }, next: any) {
+export async function handleErrors(ctx: Context, next: () => Promise<void>) {
   try {
     await next();
   } catch (err) {
-    ctx.response.status = 500;
-    ctx.response.body = { error: "Server error", message: err };
+    ctx.response.type = "json";
+    ctx.response.status = err.status;
+    ctx.response.body = { error: err, message: "Internal server error" };
   }
 }
