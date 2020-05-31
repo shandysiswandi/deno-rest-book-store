a5da37618ff1777abb6ee607974f4bbb57640b86
shandysiswandi <shandysiswandi@gmail.com>
2020-06-01 03:25:47 +0700

    implement color

--- utils/server.ts
+++ utils/server.ts
@@ -1,8 +1,8 @@
-import { Application, config } from "../deps.ts";
+import { Application, config, bgGreen, black } from "../deps.ts";
 
 const env = config();
 
 export async function start(app: Application): Promise<void> {
-  console.log(`Listening on ${env.HOST}:${env.PORT}`);
+  console.log(bgGreen(black(`Listening on ${env.HOST}:${env.PORT}`)));
   await app.listen({ port: parseInt(env.PORT) });
 }
